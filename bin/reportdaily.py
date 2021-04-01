34e04371cc6b7094f890af72e0a7d338910434bf
SchleichsSalaticus <eugen.maksymenko@suse.com>
2021-04-01 14:22:25 +0200

    add docstrings to functions

--- bin/reportdaily.py
+++ bin/reportdaily.py
@@ -4,38 +4,79 @@ add your description here
 """
 import argparse
 import sys
-
+import logging
 
 __version__ = "0.2.0"
 __author__ = "Eugen Maksymenko <eugen.maksymenko@suse.com>"
 
 
+#: The dictionary, passed to :class:`logging.config.dictConfig`,
+#: is used to setup your logging formatters, handlers, and loggers
+#: For details, see https://docs.python.org/3.4/library/logging.config.html#configuration-dictionary-schema
+DEFAULT_LOGGING_DICT = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {'format': '[%(levelname)s] %(name)s: %(message)s'},
+    },
+    'handlers': {
+        'default': {
+            'level': 'NOTSET',  # will be set later
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        __name__: {
+            'handlers': ['default'],
+            'level': 'INFO',
+            # 'propagate': True
+        }
+    }
+}
+
+
+#: Map verbosity level (int) to log level
+LOGLEVELS = {None: logging.WARNING,  # 0
+             0: logging.ERROR,
+             1: logging.WARNING,
+             2: logging.INFO,
+             3: logging.DEBUG,
+             }
+
+
 def cmd_new(args):
+    """Creates a new day for the incoming entries"""
     print("New selected", args)
     return 10
 
 
 def cmd_add(args):
+    """add a new entry"""
     print("add selected", args)
     return 0
 
 
 def cmd_change(args):
+    """change a entry by id"""
     print("Change selected", args)
     return 0
 
 
 def cmd_delete(args):
+    """delete an entry by id"""
     print("delete sected", args)
     return 0
 
 
 def cmd_list(args):
+    """list all entries of the day by id"""
     print("list selected", args)
     return 0
 
 
 def cmd_export(args):
+    """export the day by id"""
     print("export selected", args)
     return 0
 
@@ -50,6 +91,11 @@ def parsecli(cliargs=None) -> argparse.Namespace:
                                      epilog="Version %s written by %s " % (
                                          __version__, __author__)
                                      )
+
+    parser.add_argument('-v', action='count',
+                        dest="verbosity", default=0, help="Add a verbosity level for the logger  from ""-v"" to ""-vvvv""")
+
+    # subparser
     subparsers = parser.add_subparsers(help='available sub commands')
     # new cmd
     parser_new = subparsers.add_parser('new', help="creates a new day entry")
@@ -94,9 +140,18 @@ def main(cliargs=None) -> int:
     :param cliargs: Arguments to parse or None (=use :class:`sys.argv`)
     :return: error code
     """
-    args = parsecli(cliargs)
-    print(args)
-    exit_code = args.func(args)
+    exit_code = 0
+    try:
+        args = parsecli(cliargs)
+        print(args)
+        exit_code = args.func(args)
+        # If everything was good, return without error:
+        log.info("I'm an info message")
+        log.debug("I'm a debug message.")
+        log.warning("I'm a warning message.")
+        log.error("I'm an error message.")
+        log.fatal("I'm a really fatal massage!")
+
     return exit_code
 
 
